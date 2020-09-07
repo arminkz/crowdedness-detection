b63f21aa34236d713b805a17343ba34bddb9ce69
Armin Kazemi <arminkz@live.com>
2020-09-07 20:03:08 +0430

    added config file

--- sensor/main.py
+++ sensor/main.py
@@ -1,21 +1,42 @@
 import pyshark
 import concurrent.futures
+import configparser
 
-count = 0
+config = configparser.RawConfigParser()
+config.read(r'config')
+
+operation_mode = config['default']['operation_mode']
+timeout = config['default']['timeout']
+
+print(f"running in {operation_mode} mode...")
+
+unique_mac_addresses = set()
+
+
+def finalize():
+    if operation_mode == 'debug':
+        print('total packets sniffed: {}'.format(len(unique_mac_addresses)))
+        unique_mac_addresses.clear()
+    if operation_mode == 'offline':
+        print('offline')
+    if operation_mode == 'online':
+        print('online')
+
+
+def process_mac(mac_address):
+    unique_mac_addresses.add(mac_address)
+
+
 def process_pkt(packet):
-	global count
-	count += 1
-	if hasattr(packet['wlan'], 'ta'):
-		print(packet['wlan'].ta)
+    if hasattr(packet['wlan'], 'ta'):
+        process_mac(packet['wlan'].ta)
 
-t = 20
+
 print('capturing wifi communications...')
-
 capture = pyshark.LiveCapture(interface='wlan1')
 
 try:
-	capture.apply_on_packets(process_pkt,timeout=t)
+    while True:
+        capture.apply_on_packets(process_pkt, timeout=timeout)
 except concurrent.futures._base.TimeoutError as e:
-        print('total packets sniffed: {}'.format(count))
-
 
