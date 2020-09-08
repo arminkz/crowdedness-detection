e8f14966d5add2dab3d1afc7adfd3365366c531f
Armin Kazemi <arminkz@live.com>
2020-09-08 23:26:41 +0430

    added mqtt connection

--- sensor/main.py
+++ sensor/main.py
@@ -1,5 +1,5 @@
 import pyshark
-import concurrent.futures
+import paho.mqtt.client as mqtt
 import configparser
 import os
 import time
@@ -10,6 +10,9 @@ config.read(r'config')
 operation_mode = config['default']['operation_mode']
 timeout = int(config['default']['timeout'])
 interface = config['default']['interface']
+sensor_id = config['default']['sensor_id']
+mqtt_url = config['default']['mqtt_broker_url']
+mqtt_port = config['default']['mqtt_broker_port']
 
 print(f"running in {operation_mode} mode...")
 
@@ -18,6 +21,30 @@ print("putting wifi in monitor mode...")
 os.system(f'sudo ifconfig {interface} down')
 os.system(f'sudo iwconfig {interface} mode monitor')
 os.system(f'sudo ifconfig {interface} up')
+
+
+# The callback for when the client receives a CONNACK response from the server.
+def on_connect(client, userdata, flags, rc):
+    print("Connected to MQTT broker with result code "+str(rc))
+
+    # Subscribing in on_connect() means that if we lose the connection and
+    # reconnect then subscriptions will be renewed.
+    client.subscribe("$SYS/#")
+
+
+# The callback for when a PUBLISH message is received from the server.
+def on_message(client, userdata, msg):
+    print(msg.topic + " " + str(msg.payload))
+
+
+# init MQTT
+mqttc = mqtt.Client(client_id=sensor_id)
+mqttc.on_connect = on_connect
+mqttc.on_message = on_message
+
+# connect to broker
+mqttc.connect(mqtt_url, mqtt_port, 60)
+mqttc.loop_start()
 
 unique_mac_addresses = set()
 
