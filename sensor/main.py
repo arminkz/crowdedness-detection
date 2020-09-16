2eeac2275f5b2f3011a2b01319f35fc6b24f3cfd
Armin Kazemi <arminkz@live.com>
2020-09-16 12:28:27 +0430

    server connected to db

--- sensor/main.py
+++ sensor/main.py
@@ -27,10 +27,6 @@ os.system(f'sudo ifconfig {interface} up')
 def on_connect(client, userdata, flags, rc):
     print("Connected to MQTT broker with result code " + str(rc))
 
-    # Subscribing in on_connect() means that if we lose the connection and
-    # reconnect then subscriptions will be renewed.
-    # client.subscribe("$SYS/#")
-
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
--- server/app.py
+++ server/app.py
@@ -2,12 +2,55 @@ import json
 import random
 import time
 from datetime import datetime
-
+import calendar
+import pprint
 from flask import Flask, Response, render_template
+from flask_mqtt import Mqtt
+from flask_pymongo import PyMongo
 
+# init Flask
 app = Flask(__name__)
-app.config["DEBUG"] = True
+app.config['MQTT_CLIENT_ID'] = 'webserver'
+app.config['MQTT_BROKER_URL'] = '127.0.0.1'
+app.config['MQTT_BROKER_PORT'] = 1883
+app.config['MQTT_REFRESH_TIME'] = 1.0  # refresh time in seconds
+app.config['MQTT_USERNAME'] = ''
+app.config['MQTT_PASSWORD'] = ''
+app.config['MQTT_KEEPALIVE'] = 5
+app.config['MQTT_TLS_ENABLED'] = False
+app.config['MQTT_CLEAN_SESSION'] = True
+app.config["MONGO_URI"] = "mongodb://localhost:27017/crowd-db"
+
+mongo = PyMongo(app)
+
+mqtt = Mqtt(app)
+
 random.seed()  # Initialize the random number generator
+
+
+@mqtt.on_connect()
+def handle_connect(client, userdata, flags, rc):
+    print("Connected to MQTT broker with result code " + str(rc))
+    mqtt.subscribe('+/wifi')
+
+
+@mqtt.on_message()
+def handle_mqtt_message(client, userdata, message):
+    today = datetime.now()
+
+    record = mongo.db.sensor_data.find_one({"dayOfWeek": int(today.weekday()),
+                                            "dayOfMonth": int(today.strftime('%d')),
+                                            "hour": today.hour})
+    if record is None:
+        mongo.db.sensor_data.insert_one({"dayOfWeek": int(today.weekday()),
+                                         "dayOfMonth": int(today.strftime('%d')),
+                                         "hour": today.hour,
+                                         "packets": int(message.payload.decode())})
+    else:
+        mongo.db.sensor_data.update_one({"dayOfWeek": int(today.weekday()),
+                                         "dayOfMonth": int(today.strftime('%d')),
+                                         "hour": today.hour},
+                                        {"$inc": {"packets": int(message.payload.decode())}})
 
 
 @app.route('/')
@@ -23,9 +66,10 @@ def chart_data():
                 {'time': datetime.now().strftime('%Y-%m-%d %H:%M:%S'), 'value': random.random() * 100})
             yield f"data:{json_data}\n\n"
             time.sleep(1)
+
     # SSE generator
     return Response(generate_random_data(), mimetype='text/event-stream')
 
 
 if __name__ == '__main__':
-    app.run(host="localhost", port=8000, debug=True, threaded=True)
+    app.run(host="localhost", port=8000, use_reloader=False)
